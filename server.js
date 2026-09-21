5b3c159cc9f4869575b42aefebe5fb95110aa65e
azizkhan7779999997884-maker <azizkhan7779999997884@gmail.com>
2026-09-21 08:57:31 +0530

    Prepare server for cloud

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const server = http.createServer((req, res) => {
     }));
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`Instar Cloud running on port ${PORT}`);
 });
